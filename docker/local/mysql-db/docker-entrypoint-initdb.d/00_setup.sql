164616b946db4285a53f9322ffb99135bf60e6be
KatoRyota <kato.ryota.0124@gmail.com>
2021-06-20 02:59:04 +0900

    初回コミット。

--- docker/local/mysql-db/docker-entrypoint-initdb.d/00_setup.sql
+++ docker/local/mysql-db/docker-entrypoint-initdb.d/00_setup.sql
@@ -5,13 +5,13 @@ GRANT ALL ON docker.* TO docker@'%';
 
 USE docker;
 
-CREATE TABLE IF NOT EXISTS employee (
+CREATE TABLE IF NOT EXISTS customer (
     id VARCHAR(20),
     name VARCHAR(20),
     type VARCHAR(20)
 );
 
-INSERT INTO employee (id, name, type)
+INSERT INTO customer (id, name, type)
     VALUES ('ID-000-0000', 'NAME-000-0000', 'TYPE-000-0000');
-INSERT INTO employee (id, name, type)
+INSERT INTO customer (id, name, type)
     VALUES ('ID-111-1111', 'NAME-111-1111', 'TYPE-111-1111');
