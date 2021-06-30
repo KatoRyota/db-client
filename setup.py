dacf91f8695c79c4483d22803777b5033422bcf7
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-01 01:45:51 +0900

    初回コミット。

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="db-client",
+    version="0.0.1",
+    description="db-client",
+    author="Kato Ryota",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    entry_points={
+        "console_scripts": [
+            "db-client=db-client",
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ]
+)
---
+++ tests/printer/test_table_printer.py
@@ -0,0 +1,13 @@
+import unittest
+
+import mock
+
+
+class TestTablePrinter(unittest.TestCase):
+
+    def test_execute(self):
+        m = mock.MagicMock()
+
+
+if __name__ == '__main__':
+    unittest.main()
