45e248cb69a3ec61852ecda5c7649871c785a09f
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-18 10:09:31 +0900

    初回コミット。

--- tests/parser/test_oracle_parser.py
+++ tests/parser/test_oracle_parser.py
@@ -16,8 +16,16 @@ class TestOracleParser(TestCase):
         context = self._default_context()
         OracleParser(context).execute()
 
-        expected = u""
-        actual = u""
+        expected = u"ID"
+        actual = context.result_headings[0]
+        self.assertEqual(expected, actual)
+
+        expected = u"NAME"
+        actual = context.result_headings[1]
+        self.assertEqual(expected, actual)
+
+        expected = u"TYPE"
+        actual = context.result_headings[2]
         self.assertEqual(expected, actual)
 
     @staticmethod
@@ -26,6 +34,85 @@ class TestOracleParser(TestCase):
 
         context = Context()
         context.result_set_html = u'''\
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="generator" content="SQL*Plus 19.11.0">
+    <style type='text/css'>
+        body {
+            font: 10pt Arial, Helvetica, sans-serif;
+            color: black;
+            background: White;
+        }
+        p {
+            font: 10pt Arial, Helvetica, sans-serif;
+            color: black;
+            background: White;
+        }
+        table,
+        tr,
+        td {
+            font: 10pt Arial, Helvetica, sans-serif;
+            color: Black;
+            background: #f7f7e7;
+            padding: 0px 0px 0px 0px;
+            margin: 0px 0px 0px 0px;
+        }
+        th {
+            font: bold 10pt Arial, Helvetica, sans-serif;
+            color: #336699;
+            background: #cccc99;
+            padding: 0px 0px 0px 0px;
+        }
+        h1 {
+            font: 16pt Arial, Helvetica, Geneva, sans-serif;
+            color: #336699;
+            background-color: White;
+            border-bottom: 1px solid #cccc99;
+            margin-top: 0pt;
+            margin-bottom: 0pt;
+            padding: 0px 0px 0px 0px;
+        }
+        h2 {
+            font: bold 10pt Arial, Helvetica, Geneva, sans-serif;
+            color: #336699;
+            background-color: White;
+            margin-top: 4pt;
+            margin-bottom: 0pt;
+        }
+        a {
+            font: 9pt Arial, Helvetica, sans-serif;
+            color: #663300;
+            background: #ffffff;
+            margin-top: 0pt;
+            margin-bottom: 0pt;
+            vertical-align: top;
+        }
+    </style>
+    <title>SQL*Plus Report</title>
+</head>
+<body>
+    <p>
+    <table border='1' width='90%' align='center' summary='Script output'>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">NAME</th>
+            <th scope="col">TYPE</th>
+        </tr>
+        <tr>
+            <td>ID-000-0000</td>
+            <td>NAME-000-0000</td>
+            <td>TYPE-000-0000</td>
+        </tr>
+        <tr>
+            <td>ID-111-1111</td>
+            <td>NAME-111-1111</td>
+            <td>TYPE-111-1111</td>
+        </tr>
+    </table>
+    <p>2行が選択されました。<br><br>
+</body>
+</html>
 '''
         return context
 
