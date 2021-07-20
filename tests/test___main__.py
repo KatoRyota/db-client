8e4e6c0facd96c360b9189b687f1d122254e715e
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-21 02:29:18 +0900

    初回コミット。

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -17,12 +17,14 @@ class TestMain(TestCase):
         before_stdin = sys.stdin
         before_stdout = sys.stdout
         before_stderr = sys.stderr
+        before_argv = sys.argv
 
         reload(sys)
         sys.setdefaultencoding("utf-8")
         sys.stdin = before_stdin
         sys.stdout = before_stdout
         sys.stderr = before_stderr
+        sys.argv = before_argv
 
         # ---- ケース1 ----
         with mock.patch("__builtin__.reload"), \
@@ -160,6 +162,61 @@ class TestMain(TestCase):
 
             oracle_runner_execute.assert_not_called()
             mysql_runner_execute.assert_called_once()
+
+        # ---- ケース4 ----
+        with mock.patch("__builtin__.reload"), \
+                mock.patch("ConfigParser.RawConfigParser.read"), \
+                mock.patch("ConfigParser.ConfigParser.get") as config_parser_get, \
+                mock.patch("logging.config.fileConfig"), \
+                mock.patch("logging.getLogger"), \
+                mock.patch("os.makedirs"), \
+                mock.patch("dbclient.runner.oracle_runner.OracleRunner.execute") as oracle_runner_execute, \
+                mock.patch("dbclient.runner.mysql_runner.MysqlRunner.execute") as mysql_runner_execute:
+
+            config_parser_get.side_effect = self.config_parser_get_oracle_side_effect
+
+            if "dbclient.__main__" in sys.modules:
+                del sys.modules["dbclient.__main__"]
+
+            os.environ["PYTHONIOENCODING"] = "utf-8"
+            sys.argv = sys.argv + ["--display_format", "csv"]
+
+            import dbclient.__main__
+
+            expected = "utf-8"
+            actual = os.environ.get("PYTHONIOENCODING")
+            self.assertEqual(expected, actual)
+
+            expected = "csv"
+            actual = dbclient.__main__.context.display_format
+            self.assertEqual(expected, actual)
+
+            expected = ","
+            actual = dbclient.__main__.context.field_delimiter
+            self.assertEqual(expected, actual)
+
+            expected = 0
+            actual = dbclient.__main__.context.column_max_length
+            self.assertEqual(expected, actual)
+
+            expected = "off"
+            actual = dbclient.__main__.context.heading
+            self.assertEqual(expected, actual)
+
+            expected = "off"
+            actual = dbclient.__main__.context.feedback
+            self.assertEqual(expected, actual)
+
+            expected = 0
+            actual = dbclient.__main__.context.pagesize
+            self.assertEqual(expected, actual)
+
+            expected = "default"
+            actual = dbclient.__main__.context.connection_target
+            self.assertEqual(expected, actual)
+
+            oracle_runner_execute.assert_called_once()
+            mysql_runner_execute.assert_not_called()
 
     @staticmethod
     def config_parser_get_oracle_side_effect(section, option):
