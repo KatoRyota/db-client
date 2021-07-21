daedfb0270a959479655af7543fd47dbb3c797c3
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-21 14:24:22 +0900

    初回コミット。

---
+++ tests/context/test_context.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+
+import unittest
+from unittest import TestCase
+
+import mock
+
+from dbclient.context.context import Context
+
+
+class TestContext(TestCase):
+
+    def test_check_state_after_parse_option(self):
+        # type: () -> None
+
+        # ---- ケース1 ----
+        context = self._default_context()
+
+        expected = True
+        actual = context.check_state_after_parse_option()
+        self.assertEqual(expected, actual)
+
+    def test_check_state_after_execute_sql_client(self):
+        # type: () -> None
+
+        # ---- ケース1 ----
+        context = self._default_context()
+
+        expected = True
+        actual = context.check_state_after_execute_sql_client()
+        self.assertEqual(expected, actual)
+
+    def test_check_state_after_parse_sql_client_result(self):
+        # type: () -> None
+
+        # ---- ケース1 ----
+        context = self._default_context()
+
+        expected = True
+        actual = context.check_state_after_parse_sql_client_result()
+        self.assertEqual(expected, actual)
+
+    @staticmethod
+    def _default_context():
+        # type: () -> Context
+
+        context = Context()
+        context.subprocesses.append(mock.MagicMock())
+        context.subprocesses.append(mock.MagicMock())
+        context.display_format = "table"
+        context.field_delimiter = ""
+        context.column_max_length = 500
+        context.heading = "on"
+        context.feedback = "on"
+        context.pagesize = 3
+        context.connection_target = "default"
+        context.sql_client_return_code = 0
+        context.result_set_html = u""
+        context.result_headings = ["ID", "NAME", "TYPE"]
+        context.result_sets = []
+        context.result_sets.append(["ID-000-0000", "NAME-000-0000", "TYPE-000-0000"])
+        context.result_sets.append(["ID-111-1111", "NAME-111-1111", "TYPE-111-1111"])
+        context.result_sets.append(["ID-222-2222", "NAME-222-2222", "TYPE-222-2222"])
+        context.result_sets.append(["ID-333-3333", "NAME-333-3333", "TYPE-333-3333"])
+        context.result_message = u"4行が選択されました。"
+        return context
+
+
+if __name__ == "__main__":
+    unittest.main()
