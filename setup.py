7e405369db96ab69076bdb64e19045690564c8a6
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-26 02:30:29 +0900

    初回コミット。

--- setup.py
+++ setup.py
@@ -22,10 +22,9 @@ setup(
     #     "": ["requirements.txt", "README.md"],
     #     "config": ["config/application.conf", "config/logging.conf"],
     # },
-    # data_files=[
-    #     ("", ["requirements.txt", "README.md"]),
-    #     ("config", ["config/application.conf", "config/logging.conf"])
-    # ],
+    data_files=[
+        ("", ["requirements.txt", "README.md"]),
+    ],
     zip_safe=False,
     install_requires=install_requirements,
     tests_require=[],
