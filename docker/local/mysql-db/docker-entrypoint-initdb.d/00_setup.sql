a15ad78c24aa9fb7d8dd2c36e26aebba06fc446d
KatoRyota <kato.ryota.0124@gmail.com>
2021-07-28 00:51:17 +0900

    初回コミット。

--- docker/local/mysql-db/docker-entrypoint-initdb.d/00_setup.sql
+++ docker/local/mysql-db/docker-entrypoint-initdb.d/00_setup.sql
@@ -15,3 +15,23 @@ INSERT INTO customer (id, name, type)
     VALUES ('ID-000-0000', 'NAME-000-0000', 'TYPE-000-0000');
 INSERT INTO customer (id, name, type)
     VALUES ('ID-111-1111', 'NAME-111-1111', 'TYPE-111-1111');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-222-2222', 'NAME-222-2222', 'TYPE-222-2222');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-333-3333', 'NAME-333-3333', 'TYPE-333-3333');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-444-4444', 'NAME-444-4444', 'TYPE-444-4444');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-555-5555', 'NAME-555-5555', 'TYPE-555-5555');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-666-6666', 'NAME-666-6666', 'TYPE-666-6666');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-777-7777', 'NAME-777-7777', 'TYPE-777-7777');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-888-8888', 'NAME-888-8888', 'TYPE-888-8888');
+INSERT INTO customer (id, name, type)
+    VALUES ('ID-999-9999', 'NAME-999-9999', 'TYPE-999-9999');
+INSERT INTO customer (id, name, type)
+    VALUES ('あ\n' || CHR(13) || CHR(10) || 'いうえお',
+            '," ./\\' || CHR(92) || '=?!:;ヲンヰヱヴーヾ・ｧｰｭｿﾏﾞﾟ㌶Ⅲ⑳㏾☎㈱髙﨑¢£¬‖−〜―𠀋𡈽𡌛𡑮𡢽𠮟𡚴𡸴𣇄𣗄ソ能表',
+            '<<<©©©&&&');
