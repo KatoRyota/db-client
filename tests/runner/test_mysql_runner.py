6aa937f1d4c5aaead46654a096cb78d4f895920d
KatoRyota <kato.ryota.0124@gmail.com>
2021-08-09 10:20:07 +0900

    初回コミット。

--- tests/runner/test_mysql_runner.py
+++ tests/runner/test_mysql_runner.py
@@ -149,8 +149,8 @@ select * from test;
             table_printer_execute.assert_not_called()
             csv_printer_execute.assert_not_called()
 
-        # ---- ケース4 ----
-        with mock.patch("sys.stdin", new=BytesIO()), \
+        # ---- ケース3.1 ----
+        with mock.patch("sys.stdin", new=BytesIO()) as stdin, \
                 mock.patch("subprocess.Popen.__new__"), \
                 mock.patch("dbclient.context.context.Context.check_sql_execute") as context_check_sql_execute, \
                 mock.patch("dbclient.parser.mysql_parser.MysqlParser.execute") as mysql_parser_execute, \
@@ -158,43 +158,26 @@ select * from test;
                            ) as context_check_result_set_parse, \
                 mock.patch("dbclient.printer.table_printer.TablePrinter.execute") as table_printer_execute, \
                 mock.patch("dbclient.printer.csv_printer.CsvPrinter.execute") as csv_printer_execute:
-            context_check_sql_execute.return_value = False
-            context_check_result_set_parse.return_value = True
-
-            context = self._default_context()
-            context.config = self._default_config()
-            context.display_format = "csv"
-
-            with self.assertRaises(StandardError) as e:
-                MysqlRunner(context).execute()
-
-            self.assertEqual(u"SQLクライアントの実行結果が不正です。", e.exception.message)
-
-            context_check_sql_execute.assert_called_once()
-            mysql_parser_execute.assert_not_called()
-            context_check_result_set_parse.assert_not_called()
-            table_printer_execute.assert_not_called()
-            csv_printer_execute.assert_not_called()
-
-        # ---- ケース5 ----
-        with mock.patch("sys.stdin", new=BytesIO()), \
-                mock.patch("subprocess.Popen.__new__"), \
-                mock.patch("dbclient.context.context.Context.check_sql_execute") as context_check_sql_execute, \
-                mock.patch("dbclient.parser.mysql_parser.MysqlParser.execute") as mysql_parser_execute, \
-                mock.patch("dbclient.context.context.Context.check_result_set_parse"
-                           ) as context_check_result_set_parse, \
-                mock.patch("dbclient.printer.table_printer.TablePrinter.execute") as table_printer_execute, \
-                mock.patch("dbclient.printer.csv_printer.CsvPrinter.execute") as csv_printer_execute:
+            # 前提条件
             context_check_sql_execute.return_value = True
             context_check_result_set_parse.return_value = False
 
             context = self._default_context()
             context.config = self._default_config()
+            config = context.config
+
+            config.set("test", "password", "password")
+
             context.display_format = "table"
 
+            stdin.write("select * from test;\n")
+            stdin.seek(0)
+
+            # 実行
             with self.assertRaises(StandardError) as e:
                 MysqlRunner(context).execute()
 
+            # 検証
             self.assertEqual(u"SQLクライアントの実行結果の、パース処理に失敗しました。", e.exception.message)
 
             context_check_sql_execute.assert_called_once()
