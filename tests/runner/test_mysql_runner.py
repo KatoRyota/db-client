79649b76e26dffdb2bd56746b3a14b677f9928de
KatoRyota <kato.ryota.0124@gmail.com>
2021-08-09 10:29:31 +0900

    初回コミット。

--- tests/runner/test_mysql_runner.py
+++ tests/runner/test_mysql_runner.py
@@ -64,54 +64,6 @@ select * from test;
             table_printer_execute.assert_called_once()
             csv_printer_execute.assert_not_called()
 
-        # ---- ケース5.1 ----
-        with mock.patch("sys.stdin", new=BytesIO()) as stdin, \
-                mock.patch("subprocess.Popen.__new__"), \
-                mock.patch("dbclient.context.context.Context.check_sql_execute") as context_check_sql_execute, \
-                mock.patch("dbclient.parser.mysql_parser.MysqlParser.execute") as mysql_parser_execute, \
-                mock.patch("dbclient.context.context.Context.check_result_set_parse"
-                           ) as context_check_result_set_parse, \
-                mock.patch("dbclient.printer.table_printer.TablePrinter.execute") as table_printer_execute, \
-                mock.patch("dbclient.printer.csv_printer.CsvPrinter.execute") as csv_printer_execute:
-            # 前提条件
-            context_check_sql_execute.return_value = True
-            context_check_result_set_parse.return_value = True
-
-            context = self._default_context()
-            context.config = self._default_config()
-            config = context.config
-
-            config.set("test", "password", "password")
-
-            context.display_format = "csv"
-
-            stdin.write("select * from test;\n")
-            stdin.seek(0)
-
-            # 実行
-            MysqlRunner(context).execute()
-
-            # 検証
-            actual = os.environ.get("MYSQL_PWD")
-            expected = "password"
-            self.assertEqual(expected, actual)
-
-            actual = context.sql
-            expected = u"""\
-select * from test;
-"""
-            self.assertEqual(expected, actual)
-
-            actual = context.dsn
-            expected = "-h host -P port -D database_name -u user_name"
-            self.assertEqual(expected, actual)
-
-            context_check_sql_execute.assert_called_once()
-            mysql_parser_execute.assert_called_once()
-            context_check_result_set_parse.assert_called_once()
-            table_printer_execute.assert_not_called()
-            csv_printer_execute.assert_called_once()
-
         # ---- ケース2.1 ----
         with mock.patch("sys.stdin", new=BytesIO()) as stdin, \
                 mock.patch("subprocess.Popen.__new__"), \
@@ -185,6 +137,54 @@ select * from test;
             context_check_result_set_parse.assert_called_once()
             table_printer_execute.assert_not_called()
             csv_printer_execute.assert_not_called()
+
+        # ---- ケース5.1 ----
+        with mock.patch("sys.stdin", new=BytesIO()) as stdin, \
+                mock.patch("subprocess.Popen.__new__"), \
+                mock.patch("dbclient.context.context.Context.check_sql_execute") as context_check_sql_execute, \
+                mock.patch("dbclient.parser.mysql_parser.MysqlParser.execute") as mysql_parser_execute, \
+                mock.patch("dbclient.context.context.Context.check_result_set_parse"
+                           ) as context_check_result_set_parse, \
+                mock.patch("dbclient.printer.table_printer.TablePrinter.execute") as table_printer_execute, \
+                mock.patch("dbclient.printer.csv_printer.CsvPrinter.execute") as csv_printer_execute:
+            # 前提条件
+            context_check_sql_execute.return_value = True
+            context_check_result_set_parse.return_value = True
+
+            context = self._default_context()
+            context.config = self._default_config()
+            config = context.config
+
+            config.set("test", "password", "password")
+
+            context.display_format = "csv"
+
+            stdin.write("select * from test;\n")
+            stdin.seek(0)
+
+            # 実行
+            MysqlRunner(context).execute()
+
+            # 検証
+            actual = os.environ.get("MYSQL_PWD")
+            expected = "password"
+            self.assertEqual(expected, actual)
+
+            actual = context.sql
+            expected = u"""\
+select * from test;
+"""
+            self.assertEqual(expected, actual)
+
+            actual = context.dsn
+            expected = "-h host -P port -D database_name -u user_name"
+            self.assertEqual(expected, actual)
+
+            context_check_sql_execute.assert_called_once()
+            mysql_parser_execute.assert_called_once()
+            context_check_result_set_parse.assert_called_once()
+            table_printer_execute.assert_not_called()
+            csv_printer_execute.assert_called_once()
 
     @staticmethod
     def _default_config():
